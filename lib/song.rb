099d53e03e704d05632a2e7308e318b63d9292ed
apradhn <abhijit.pradhan6@gmail.com>
2015-06-12 12:39:58 -0400

    all done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,21 @@
+require 'tempfile'
+require 'pry'
+require 'pathname'
+
 class Song
-  # code goes here
+  attr_accessor :title
+  attr_reader :artist
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def serialize
+    file_name = @title.gsub(" ", "_").downcase+".txt"
+    file = File.new("./tmp/#{file_name}", "w+")
+    open(file, 'w') { |f|
+      f.puts "#{artist.name} - #{title}"
+    }
+    file
+  end
 end
